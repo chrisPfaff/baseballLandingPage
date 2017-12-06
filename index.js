7e51884ce531a9fa38a51b35587eacf1c92fac00
Chris Pfaff <chrispfaff10@gmail.com>
2017-12-06 15:59:14 -0500

    .mov landing page for ficticious baseball team

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const port = 3000;
+
+app.use(express.static('public'))
+
+
+app.get('/' , (req, res) => {
+  res.sendFile(path.join(__dirname + "/main.html"));
+})
+
+
+app.listen(port, () => {
+  console.log('Your server is running at port 3000');
+})
