dcda5bf027e68c7961f13b1e62bf1a054cbd014b
Chris Pfaff <chrispfaff10@gmail.com>
2018-11-27 15:39:39 -0600

    added procfile

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path')
+const path = require("path");
 const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-
-app.get('/' , (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/main.html"));
-})
+});
 
-
-app.listen(port, () => {
-  console.log('Your server is running at port 3000');
-})
+app.listen(process.env.PORT || port, () => {
+  console.log("Your server is running at port 3000");
+});
